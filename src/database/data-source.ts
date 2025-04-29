dcdd3b16210a421d9dbee285300b17076a6ea5ef
Rodrigo Lopes <rodrigo.plopesti@gmail.com>
2025-04-29 10:36:59 -0300

    fix: correct environment variable name for PostgreSQL user in docker-compose and update data source configuration

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -4,14 +4,17 @@ import { Summarize } from "../entities/Summarize.entity"
 import { CreateUserTable1745889423991 } from "./migrations/1745889423991-CreateUserTable"
 import User from "../entities/User.entity"
 import { AddUserIdReferenceInSummarizeTable1745929975091 } from "./migrations/1745929975091-AddUserIdReferenceInSummarizeTable"
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 5432,
-    username: process.env.DB_USERNAME || "test",
-    password: process.env.DB_PASSWORD || "test",
-    database: process.env.DB_NAME || "summarize_db",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: false,
     migrations: [
         CreateSummarizeTable1745721184412, 
